344d498230113f652324fe4125707f3e3697a13c
Pedro Pereira <pedropereirex@gmail.com>
2018-04-03 15:45:23 +0000

    Fix Blobs script

--- z5-vendor.mk
+++ z5-vendor.mk
@@ -1,12 +1,12 @@
 #Vendor Blobs for Leagoo Z5 (Updated 03/04/2018)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/bin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/proprietary/bin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/vendor/lib,system/vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leagoo/z5/proprietary/vendor/lib,system/vendor/lib)
 
